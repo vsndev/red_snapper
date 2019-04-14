da0d635c1daf7cf544716ac7192fb6854b302d34
w-kondo <wata_82_10@yahoo.co.jp>
2019-04-14 17:41:58 +0900

    toppageのlink設定

--- red_snapper_pj/red_snapper_app/views.py
+++ red_snapper_pj/red_snapper_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def ShowLogin(request):
     return render(request, "login.html")
+
+
+def ShowToppage(request):
+    return render(request, "toppage.html")
