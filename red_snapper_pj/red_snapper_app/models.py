0aad24de15c9d9ca96d7d0ad6924ff8ac52d7a95
sae_farbenfroh <saeinasaki0701@gmail.com>
2019-04-17 22:58:22 +0900

    Register/DBcreate

--- red_snapper_pj/red_snapper_app/models.py
+++ red_snapper_pj/red_snapper_app/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class RegisterData(models.Model):
     member_name = models.CharField(max_length=20)
     doc_title = models.CharField(max_length=50)
-    deadline_date= models.DateField()
+    deadline_date= models.DateField(default=datetime.date.today)
 
+    def __str__(self):
+        return'&lt;Register:name=' + str(self.id) + ',' +self.member_name + ',' + self.doc_title 
 
--- red_snapper_pj/red_snapper_app/views.py
+++ red_snapper_pj/red_snapper_app/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import CreateView
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+#from django.views.generic import CreateView
+#from django.views.generic.edit import CreateView
 from .models import RegisterData
 from .forms import RegisterForm
-
+from django.shortcuts import redirect
 
 # Create your views here.
 def ShowLogin(request):
@@ -15,9 +14,24 @@ def ShowLogin(request):
 def ShowToppage(request):
     return render(request, "toppage.html")
 
+#削除予定
+#class CreateRegisterView(CreateView):
+    #model = RegisterData
+    #form_class = RegisterForm
+    #template_name = 'register.html'
+    #success_url = "/"
 
-class CreateRegisterView(CreateView):
-    model = RegisterData
-    form_class = RegisterForm
-    template_name = 'register.html'
-    success_url = "/"
+def ShowRegister(request):
+
+    if request.method == 'POST':
+        model = RegisterData()
+        register = RegisterForm(request.POST, instance = model)
+        register.save()
+        return redirect(to='/register')
+    
+    #検証用削除予定
+    params = {
+        'title': 'Guten Morgen',
+        'form': RegisterForm(),
+    }
+    return render(request,'register.html',params)
