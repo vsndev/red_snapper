c466efbea57add57006d51c71b6c16988e08178a
sae_farbenfroh <saeinasaki0701@gmail.com>
2019-04-17 23:37:36 +0900

    edit models, initialize DB table

--- red_snapper_pj/red_snapper_app/views.py
+++ red_snapper_pj/red_snapper_app/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from django.views.generic import CreateView
-#from django.views.generic.edit import CreateView
 from .models import RegisterData
 from .forms import RegisterForm
 from django.shortcuts import redirect
@@ -14,13 +12,6 @@ def ShowLogin(request):
 def ShowToppage(request):
     return render(request, "toppage.html")
 
-#削除予定
-#class CreateRegisterView(CreateView):
-    #model = RegisterData
-    #form_class = RegisterForm
-    #template_name = 'register.html'
-    #success_url = "/"
-
 def ShowRegister(request):
 
     if request.method == 'POST':
@@ -29,9 +20,7 @@ def ShowRegister(request):
         register.save()
         return redirect(to='/register')
     
-    #検証用削除予定
     params = {
-        'title': 'Guten Morgen',
         'form': RegisterForm(),
     }
     return render(request,'register.html',params)
